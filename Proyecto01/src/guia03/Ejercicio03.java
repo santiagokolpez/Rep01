478fe7018a33708d647a8f18f534b1af47094263
Santiago <santiagolopezdabat@gmail.com>
2023-03-26 02:42:53 -0300

    hasta el 03 de la g03, sábado a la noche

--- Proyecto01/src/guia03/Ejercicio03.java
+++ Proyecto01/src/guia03/Ejercicio03.java
@@ -5,8 +5,20 @@
     Nota: investigar la función Lenght() en Java. */
 package guia03;
 
+import java.util.Scanner;
+
 public class Ejercicio03 {
     public static void main(String[] args) {
+        System.out.println("Ingrese una frase o palabra de 8 de largo");
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        String fraseopalabra = leer.next();
+       
+         if (fraseopalabra.length() == 8) {   
+           System.out.println("Correcto");
+       }  else {  //sino
+           System.out.println("Incorrecto");
+       }
+        
         
     }
 }
